d4fe26013b831b6ca3d0ea0972ac998907b3515c
tanzi <1107360+qqblack@user.noreply.gitee.com>
2024-03-24 08:31:09 +0800

    make it more simple

--- CSharp/NModbus_WriteSingleRegister/Controller.cs
+++ CSharp/NModbus_WriteSingleRegister/Controller.cs
@@ -12,24 +12,8 @@ namespace NModbusTest
             Controller dM2C = new Controller();
             dM2C.Connect(serialNo: 11, BaudRate: 38400);
 
-            Console.WriteLine("Step1...");
+            Console.WriteLine("Started...");
             bool bSucceed = dM2C.SendPosNo(slaveId, 0);
-            Console.WriteLine($"Completed. Succeed:{bSucceed}");
-
-            Console.WriteLine("Step2...");
-            bSucceed = dM2C.SendPosNo(slaveId, 1);
-            Console.WriteLine($"Completed. Succeed:{bSucceed}");
-
-            Console.WriteLine("Step3...");
-            bSucceed = dM2C.SendPosNo(slaveId, 0);
-            Console.WriteLine($"Completed. Succeed:{bSucceed}");
-
-            Console.WriteLine("Step4...");
-            bSucceed = dM2C.SendPosNo(slaveId, 1);
-            Console.WriteLine($"Completed. Succeed:{bSucceed}");
-
-            Console.WriteLine("Step5...");
-            bSucceed = dM2C.SendPosNo(slaveId, 0);
             Console.WriteLine($"Completed. Succeed:{bSucceed}");
         }
     }
