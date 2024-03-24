e90c8a1c9273b8fcc26160c5874d23ee7130adc4
tanzi <1107360+qqblack@user.noreply.gitee.com>
2024-03-24 08:25:13 +0800

    Turn the demo to single-file

--- CSharp/NModbus_WriteSingleRegister/Controller.cs
+++ CSharp/NModbus_WriteSingleRegister/Controller.cs
@@ -4,6 +4,36 @@ using System.IO.Ports;
 
 namespace NModbusTest
 {
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            byte slaveId = 1;
+            Controller dM2C = new Controller();
+            dM2C.Connect(serialNo: 11, BaudRate: 38400);
+
+            Console.WriteLine("Step1...");
+            bool bSucceed = dM2C.SendPosNo(slaveId, 0);
+            Console.WriteLine($"Completed. Succeed:{bSucceed}");
+
+            Console.WriteLine("Step2...");
+            bSucceed = dM2C.SendPosNo(slaveId, 1);
+            Console.WriteLine($"Completed. Succeed:{bSucceed}");
+
+            Console.WriteLine("Step3...");
+            bSucceed = dM2C.SendPosNo(slaveId, 0);
+            Console.WriteLine($"Completed. Succeed:{bSucceed}");
+
+            Console.WriteLine("Step4...");
+            bSucceed = dM2C.SendPosNo(slaveId, 1);
+            Console.WriteLine($"Completed. Succeed:{bSucceed}");
+
+            Console.WriteLine("Step5...");
+            bSucceed = dM2C.SendPosNo(slaveId, 0);
+            Console.WriteLine($"Completed. Succeed:{bSucceed}");
+        }
+    }
+
     public class Controller
     {
         private IModbusMaster? master;
